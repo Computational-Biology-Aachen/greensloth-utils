5c3a7ce052ad8cce18535c67e47734dfba9e6fb0
Elouen_Corvest <elouen.corvest@rwth-aachen.de>
2026-06-09 15:39:02 +0200

    dev: new: started makind model editting

--- src/GreenSlothUtils/__main__.py
+++ src/GreenSlothUtils/__main__.py
@@ -19,17 +19,26 @@ from textual.widgets import (
     Header,
     Input,
     Label,
+    DataTable
 )
 
 from GreenSlothUtils import installerfuncs
 
 
+def reset_button(button: Button, label: str, color: str) -> None:
+    button.label = label
+    button.variant = color
+
 class ModelPathSelection(DirectoryTree):
     def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
         return [path for path in paths if not path.name.startswith(".")]
 class GreenSlothInitialize(App):
-    """A Textual app to manage stopwatches."""
+    """A Textual app fascilitate the model initialization process of GreenSloth"""
     
+    def __init__(self):
+        super().__init__()
+        self.selected_model_path = None
+
     CSS_PATH = "app.tcss"
 
     BINDINGS = [
@@ -42,7 +51,7 @@ class GreenSlothInitialize(App):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
-        with ContentSwitcher(initial="start-page"):
+        with ContentSwitcher(initial="start-page", id="content-mains"):
             with Vertical(id="start-page"):
                 yield Button("Create New Model", id="button-switchpage-createmodel")
                 yield Button("Edit Existing Model", id="button-switchpage-editmodel")
@@ -64,10 +73,50 @@ class GreenSlothInitialize(App):
                 
             with Vertical(id="editmodel-select-page"):
                 yield HorizontalGroup(
-                    Label("Please select a Model directory", id="directory-select-label"),
+                    Label("Please select a Model directory", id="label-directoryselect"),
                     Button("Select", id="button-modelselect", variant="default", disabled=True),
                 )
-                yield ModelPathSelection("./", id="create-model-directory")
+                yield ModelPathSelection("./", id="directory-editmodel")
+                
+            with Vertical(id="editmodel-functions-page"):
+                yield Label("Model Name", id="label-editmodel-name")
+                with ContentSwitcher(initial="editmodel-functions-buttons", id="content-editmodel-functions"):
+                    with Vertical(id="editmodel-functions-buttons"):
+                        yield HorizontalGroup(
+                            Label("Extract Information from Model"),
+                            Button("Extract", id="button-editmodel-modelinfo", variant="default")
+                        )
+                        yield HorizontalGroup(
+                            Label("Compare Information with Model"),
+                            Button("Compare", id="button-editmodel-compareinfo", variant="default")
+                        )
+                        yield HorizontalGroup(
+                            Label("Update Information from Main Glossaries"),
+                            Button("Update", id="button-editmodel-glossary", variant="default")
+                        )
+                        yield HorizontalGroup(
+                            Label("Convert to Python"),
+                            Button("Convert", id="button-editmodel-convertpython", variant="default")
+                        )
+                        yield HorizontalGroup(
+                            Label("Convert to LaTeX"),
+                            Button("Convert", id="button-editmodel-convertlatex", variant="default")
+                        )
+                    with Vertical(id="editmodel-functions-compare"):
+                        yield HorizontalGroup(
+                            Button("Back", id="button-editmodel-compare-back"),
+                            Button("Extract and Compare", id="button-editmodel-compareagain", variant="default")
+                        )
+                        yield HorizontalGroup(
+                            VerticalScroll(
+                                Label("Variables", classes="title-compare"),
+                                DataTable(id="datatable-variables")
+                            ),
+                            VerticalScroll(
+                                Label("Parameters", classes="title-compare"),
+                                DataTable(id="datatable-parameters")
+                            )
+                        )
 
 
     def action_toggle_dark(self) -> None:
@@ -80,19 +129,19 @@ class GreenSlothInitialize(App):
         self.exit()
         
     def action_starting_page(self) -> None:
-        switcher = self.query_one(ContentSwitcher)
+        switcher = self.query_one("#content-mains", ContentSwitcher)
         switcher.current = "start-page"
 
     #### Buttons
     
     @on(Button.Pressed, "#button-switchpage-createmodel")
     def switch_to_create_model(self) -> None:
-        switcher = self.query_one(ContentSwitcher)
+        switcher = self.query_one("#content-mains", ContentSwitcher)
         switcher.current = "create-model-page"
 
     @on(Button.Pressed, "#button-switchpage-editmodel")
-    def switch_to_edit_model(self) -> None:
-        switcher = self.query_one(ContentSwitcher)
+    def switch_to_editmodel_select(self) -> None:
+        switcher = self.query_one("#content-mains", ContentSwitcher)
         switcher.current = "editmodel-select-page"
     
     @on(Button.Pressed, "#button-modelcreate")
@@ -118,6 +167,71 @@ class GreenSlothInitialize(App):
             create_button.label = "Create"
             installerfuncs.gs_install(model_name, Path(model_path))
             directory_tree.reload()
+            
+    @on(Button.Pressed, "#button-modelselect")
+    def switch_to_editmodel_funcs(self) -> None:
+        switcher = self.query_one(ContentSwitcher)
+        selected_dir = Path(self.query_one("#label-directoryselect", Label).content)
+        self.app.selected_model_path = selected_dir
+        
+        label_modelname = self.query_one("#label-editmodel-name", Label)
+        label_modelname.update(selected_dir.name)
+        
+        switcher.current = "editmodel-functions-page"
+        
+    @on(Button.Pressed, "#button-editmodel-modelinfo")
+    def extract_model_info(self) -> None:
+        
+        model_path = self.app.selected_model_path
+        model_name = self.app.selected_model_path.name
+        
+        installerfuncs.gs_extractinfo(
+            model_dir=model_path,
+            model_name=model_name
+        )
+        
+        button = self.query_one("#button-editmodel-modelinfo", Button)
+        button.variant = "success"
+        button.label = "Extracted!"
+        
+        self.set_timer(3, lambda: reset_button(button, "Extract", "default"))
+        
+    @on(Button.Pressed, "#button-editmodel-compareinfo")
+    def compare_model_info(self) -> None:
+        switcher = self.query_one("#content-editmodel-functions", ContentSwitcher)
+        switcher.current = "editmodel-functions-compare"
+        
+        compare_dict = installerfuncs.gs_compareinfos(
+            model_dir=self.app.selected_model_path
+        )
+        
+        table = self.query_one(f"#datatable-variables", DataTable)
+        table.add_columns("Model", "Glossary")
+        
+        if len(compare_dict["variables"][0]) != 0 or len(compare_dict["variables"][1]) != 0:
+            for var in compare_dict["variables"][0]:
+                table.add_row(var, "")
+            for var in compare_dict["variables"][1]:
+                table.add_row("", var)
+
+        # for name, (checked_model, checked_gloss) in compare_dict.items():
+        #     table = self.query_one(f"#datatable-{name}", DataTable)
+        #     table.add_columns("Model", "Glossary")
+            
+        #     if len(checked_model) != 0 or len(checked_gloss) != 0:
+        #         for var in checked_model:
+        #             table.add_row(var, "")
+        #         for var in checked_gloss:
+        #             table.add_row("", var)
+
+    @on(Button.Pressed, "#button-editmodel-compare-back")
+    def goback_editmodel_functions(self) -> None:
+        switcher = self.query_one("#content-editmodel-functions", ContentSwitcher)
+        switcher.current = "editmodel-functions-buttons"
+        
+    @on(Button.Pressed, "#button-editmodel-compareagain")
+    def compare_again(self) -> None:
+        self.compare_model_info()
 
     ### Input Events
     
@@ -140,9 +254,26 @@ class GreenSlothInitialize(App):
     ### Directory Tree Events
     
     @on(DirectoryTree.DirectorySelected, "#create-model-directory")
-    def update_directory_label(self, event: DirectoryTree.DirectorySelected) -> None:
+    def update_create_model_directory_label(self, event: DirectoryTree.DirectorySelected) -> None:
         """Update the directory label with the selected path."""
-        self.query_one("#directory-label", Label).update(str(event.path) + "/")
+        selected_dir = event.path
+        self.query_one("#directory-label", Label).update(str(selected_dir) + "/")
+    
+    @on(DirectoryTree.DirectorySelected, "#directory-editmodel")
+    def edit_model_directory_select(self, event: DirectoryTree.DirectorySelected) -> None:
+        """Update the directory label with the selected path."""
+        
+        selected_dir = event.path
+        select_button = self.query_one("#button-modelselect", Button)
+        
+        self.query_one("#label-directoryselect", Label).update(str(selected_dir) + "/")
+        
+        if Path.exists(selected_dir / "model_info"):
+            select_button.variant = "success"
+            select_button.disabled = False
+        else:
+            select_button.variant = "default"
+            select_button.disabled = True
 
 if __name__ == "__main__":
     app = GreenSlothInitialize()
--- src/GreenSlothUtils/installerfuncs.py
+++ src/GreenSlothUtils/installerfuncs.py
@@ -1,7 +1,13 @@
 import os
 from pathlib import Path
 from typing import Optional
-import click
+import importlib.util
+from inspect import getmembers, isfunction
+from mxlpy import Model
+import sys
+import pandas as pd
+
+from GreenSlothUtils import extract_select_to_gloss
 
 def iterate_files(path_to_scan: Path, model_name: str, target_dir: Path, dirs = "") -> None:
 
@@ -29,25 +35,152 @@ def iterate_files(path_to_scan: Path, model_name: str, target_dir: Path, dirs = 
                 f.write(content)
 
 
+def get_model(model_dir: Path, model_name: str) -> Model | None:
+    spec = importlib.util.spec_from_file_location("model", model_dir / "model" / "__init__.py")
+    if spec is None or spec.loader is None:
+        msg = f"Cannot find spec for module {model_name}"
+        raise ImportError(msg)
+
+    module = importlib.util.module_from_spec(spec)
+    sys.modules["model"] = module
+    spec.loader.exec_module(module)
+
+    for func in getmembers(module, isfunction):
+        if func[0] == model_name:
+            return func[1]()
+    return None
+
 def gs_install(
     model_name: str,
     target_dir: Optional[Path] = None,
-):
+) -> None:
     if target_dir is None:
         target_dir = Path('./')
 
     try:
-        os.makedirs(
+        Path.mkdir(
             target_dir / model_name,
+            parents=True,
             exist_ok=False
         )
     except FileExistsError:
-        raise FileExistsError(f"There already exists a folder with name '{model_name}'")
+        msg = f"There already exists a folder with name '{model_name}'"
+        raise FileExistsError(msg) from None
 
-    iterate_files(Path(__file__).parent / 'modelinit', model_name, target_dir= target_dir / model_name)
-    
+    iterate_files(Path(__file__).parent / "modelinit", model_name, target_dir= target_dir / model_name)
+
     for i in ["model_glosses", "python_written/gloss_to_python", "python_written/model_to_latex"]:
         try:
             Path.mkdir(target_dir / model_name / "model_info" / i, parents=True, exist_ok=True)
         except FileExistsError:
-            raise FileExistsError(f"Something went wrong!")
+            msg_0 = "Something went wrong!"
+            raise FileExistsError(msg_0) from None
+
+def gs_extractinfo(
+    model_dir: Path,
+    model_name: str
+) -> None:
+    
+    modelinfo_dir = model_dir / "model_info"
+    modelgloss_dir = modelinfo_dir / "model_glosses"
+    
+    model = get_model(
+        model_dir=model_dir,
+        model_name=model_name
+    )
+    
+    extract_select_to_gloss(
+            select=model.get_raw_variables(),
+            column_names=[
+                "Name",
+                "Common Abbr.",
+                "Paper Abbr.",
+                "KEGG ID",
+                "Python Var",
+                "Glossary ID",
+            ],
+            pythonvar_col="Python Var",
+            path_to_write= modelgloss_dir / "model_comps.txt",
+        )
+    
+    extract_select_to_gloss(
+            select=model.get_parameter_values(),
+            column_names=[
+                "Short Description",
+                "Common Abbr.",
+                "Paper Abbr.",
+                "Value",
+                "Unit",
+                "Python Var",
+                "Reference",
+            ],
+            pythonvar_col="Python Var",
+            path_to_write= modelgloss_dir / "model_params.txt",
+            value_col="Value"
+        )
+    
+    surr_outputs_dict = {}
+    for surr in model.get_raw_surrogates().values():
+        for output in surr.outputs:
+            surr_outputs_dict[output] = None
+    full_dict = model.get_derived_variables() | surr_outputs_dict | model.get_raw_readouts()
+    extract_select_to_gloss(
+        select=full_dict,
+        column_names=[
+            "Name",
+            "Common Abbr.",
+            "Paper Abbr.",
+            "KEGG ID",
+            "Python Var",
+            "Glossary ID",
+        ],
+        pythonvar_col="Python Var",
+        path_to_write= modelgloss_dir / "model_derived_comps.txt",
+    )
+
+    extract_select_to_gloss(
+        select=model.get_derived_parameters(),
+        column_names=["Short Description", "Common Abbr.", "Paper Abbr.", "Python Var"],
+        pythonvar_col="Python Var",
+        path_to_write= modelgloss_dir / "model_derived_params.txt",
+    )
+    
+    extract_select_to_gloss(
+        select=model._reactions,
+        column_names=[
+            "Short Description",
+            "Common Abbr.",
+            "Paper Abbr.",
+            "KEGG ID",
+            "Python Var",
+            "Glossary ID",
+        ],
+        pythonvar_col="Python Var",
+        path_to_write= modelgloss_dir / "model_rates.txt",
+    )
+    
+def gs_compareinfos(
+    model_dir: Path
+) -> dict:
+    
+    res_dict = {}
+    
+    modelinfo_dir = model_dir / "model_info"
+    modelgloss_dir = modelinfo_dir / "model_glosses"
+    
+    for key, name in zip(["comps", "rates", "params", "derived_comps", "derived_params"], ["variables", "rates", "parameters", "derivedvars", "derivedparams"]):
+        from_model=modelgloss_dir / f"model_{key}.csv"
+        edit_gloss=modelinfo_dir / f"{key}.csv"
+        
+        df_model = pd.read_csv(from_model, keep_default_na=False)
+        df_gloss = pd.read_csv(edit_gloss, keep_default_na=False)
+        
+        checked_model = set(df_model["Python Var"]) - set(df_gloss["Python Var"])
+        checked_gloss = set(df_gloss["Python Var"]) - set(df_model["Python Var"])
+        
+        if len(checked_model) != 0 or len(checked_gloss) != 0:
+            res_dict[name] = (checked_model, checked_gloss)
+        else:
+            res_dict[name] = (set(), set())
+        
+    return res_dict
